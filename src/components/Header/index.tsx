d15b63fe3580e6d9bdb8f8e5c7ed0fbbbd8076ca
Matheus Aguiar <first_mf@hotmail.com>
2021-04-13 16:53:44 -0300

    refact Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,16 @@
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
 
-export function Header() {
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
     return(
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money"/>
-                <button>Nova transação</button>
+                <button onClick={onOpenNewTransactionModal}>Nova transação</button>
             </Content>
         </Container>
     )
